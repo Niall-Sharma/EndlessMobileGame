6c6ba19f783fb38d305d2ca789e657068745f1e8
Niall Sharma <Niall.k.sharma@gmail.com>
2023-09-30 16:39:30 -0500

    working audio

--- MainMenu/options_menu_box.gd
+++ MainMenu/options_menu_box.gd
@@ -1,5 +1,10 @@
 extends VBoxContainer
 
+var Audio 
+
+func  _ready():
+	AudioServer.set_bus_layout(load("res://default_bus_layout.tres"))
+	$VolumeBox/Volume.value = AudioServer.get_bus_volume_db(0)
 
 func _on_back_pressed():
 	get_parent().get_child(1).show()
@@ -10,3 +15,11 @@ func _on_data_wipe_button_pressed():
 	$DataWipeButton.text = "DATA WIPED"
 	$DataWipeButton.disabled = true
 	Highscore.destroy_data()
+
+
+func _on_volume_value_changed(value):
+	AudioServer.set_bus_volume_db(0, value)
+	if value == -20:
+		AudioServer.set_bus_mute(0, true)
+	else:
+		AudioServer.set_bus_mute(0, false)
